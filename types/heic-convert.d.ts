7a5dc8bd805a848acb7e50f5d18684c16c564280
Samuel Goldsmith <samuelrg2005@gmail.com>
2025-07-14 21:58:35 -0400

    test

--- types/heic-convert.d.ts
+++ types/heic-convert.d.ts
@@ -1,19 +1,19 @@
-// declare module "heic-convert" {
-//   interface ConversionOptions {
-//     buffer: ArrayBuffer;
-//     format: "JPEG" | "PNG";
-//     quality?: number;
-//   }
-
-//   function heicConvert(options: ConversionOptions): Promise<ArrayBuffer>;
-
-//   export = heicConvert;
-// }
 declare module "heic-convert" {
-  export = heicConvert;
-  function heicConvert(options: {
+  interface ConversionOptions {
     buffer: Buffer;
     format: "JPEG" | "PNG";
     quality?: number;
-  }): Promise<Buffer>;
+  }
+
+  function heicConvert(options: ConversionOptions): Promise<Buffer>;
+
+  export = heicConvert;
 }
+// declare module "heic-convert" {
+//   export = heicConvert;
+//   function heicConvert(options: {
+//     buffer: Buffer;
+//     format: "JPEG" | "PNG";
+//     quality?: number;
+//   }): Promise<Buffer>;
+// }
